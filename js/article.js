906ed831c313772793ac9ecc2ebbe03580434f17
Nooaah <noah.chtl@gmail.com>
2020-02-10 15:05:02 +0100

    end bro 🔥

--- js/article.js
+++ js/article.js
@@ -3,4 +3,15 @@ jQuery(document).ready(function () {
     document.getElementById('h1Title').textContent = sessionStorage.title.charAt(0).toUpperCase() + sessionStorage.title.slice(1);
     document.getElementById('article').textContent = sessionStorage.body.charAt(0).toUpperCase() + sessionStorage.body.slice(1);
 
+    $.ajax({
+        method: 'GET',
+        url: 'https://jsonplaceholder.typicode.com/albums/' + sessionStorage.id + '/photos',
+        success: function (result) {
+            console.log(result[0].thumbnailUrl);
+
+            document.querySelector('img').src = result[0].thumbnailUrl;
+
+        }
+    });
+
 });
